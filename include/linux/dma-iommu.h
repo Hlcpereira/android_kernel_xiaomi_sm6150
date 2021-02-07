309681c6ca2c88a90bfcee036277796798695c09
Henrique Pereira <hlcpereira@pixelexperience.org>
2021-02-07 18:04:20 -0300

    [RECHECK ME] drivers: iommu: Import Xiaomi changes
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- include/linux/dma-iommu.h
+++ include/linux/dma-iommu.h
@@ -74,6 +74,7 @@ int iommu_dma_mapping_error(struct device *dev, dma_addr_t dma_addr);
 /* The DMA API isn't _quite_ the whole story, though... */
 void iommu_dma_map_msi_msg(int irq, struct msi_msg *msg);
 void iommu_dma_get_resv_regions(struct device *dev, struct list_head *list);
+int iommu_dma_set(struct device *dev, const char *name, bool best_fit);
 
 #else
 
@@ -106,6 +107,10 @@ static inline void iommu_dma_map_msi_msg(int irq, struct msi_msg *msg)
 
 static inline void iommu_dma_get_resv_regions(struct device *dev, struct list_head *list)
 {
+}
+int iommu_dma_set(struct device *dev, const char *name, bool best_fit)
+{
+	return 0;
 }
 
 #endif	/* CONFIG_IOMMU_DMA */
